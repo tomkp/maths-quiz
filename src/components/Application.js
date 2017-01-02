b507e1ff29a70c45a3569118050be82c17ffdad0
tomkp <tom@tomkp.com>
2017-01-02 13:11:36 +0000

    ...

---
+++ src/components/Application.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './application.css';
+
+import Questions from './Questions';
+
+
+const Application = () => {
+    return (
+        <div>
+            <h2>🌈 😁  💯 Maths Quiz! 💯  😁 🌈</h2>
+            <Questions />
+        </div>
+    )
+};
+
+export default Application;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './styles.css';
-import './application.css';
 
-import Questions from './components/Questions';
+import Application from './components/Application';
 
-
-const Application = () => {
-    return (
-        <div>
-            <h2>🌈 😁  💯 Maths Quiz! 💯  😁 🌈</h2>
-            <Questions />
-        </div>
-    )
-};
 
 render(<Application />, document.getElementById('root'));
